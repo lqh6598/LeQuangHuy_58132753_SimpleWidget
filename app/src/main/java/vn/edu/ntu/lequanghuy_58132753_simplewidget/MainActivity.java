52cb3b3c7d397e1fa1b0b588526220966a993a56
Quang Huy <lqh6598@gmail.com>
2020-04-21 21:32:49 +0700

    v1.0

---
+++ app/src/main/java/vn/edu/ntu/lequanghuy_58132753_simplewidget/MainActivity.java
@@ -0,0 +1,14 @@
+package vn.edu.ntu.lequanghuy_58132753_simplewidget;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
